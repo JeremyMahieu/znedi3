28ef0a64e98cb2607c28fe109f9fc4fc0e16fe67
sekrit-twc <noreply@example.com>
2021-10-16 19:21:41 -0700

    Pipeline all operations with graphengine

--- znedi3/znedi3_impl.h
+++ znedi3/znedi3_impl.h
@@ -34,11 +34,32 @@ class znedi3_filter : public ::znedi3_filter {
 	PixelType m_type;
 	CPUClass m_cpu;
 public:
-	explicit znedi3_filter(const NNEDI3Weights &weights, const znedi3_filter_params &params);
+	znedi3_filter(const NNEDI3Weights &weights, const znedi3_filter_params &params);
 
 	size_t get_tmp_size(unsigned width, unsigned height) const;
 
 	void process(unsigned width, unsigned height, const void *src, ptrdiff_t src_stride, void *dst, ptrdiff_t dst_stride, void *tmp, unsigned parity) const;
+};
+
+class znedi3_filter2 : public ::znedi3_filter {
+	struct filters;
+	struct data;
+
+	std::unique_ptr<filters> m_filters;
+	std::unique_ptr<data> m_data_t;
+	std::unique_ptr<data> m_data_b;
+
+	std::unique_ptr<filters> setup_filters(const NNEDI3Weights &weights, const znedi3_filter_params &params, unsigned width, unsigned height);
+
+	std::unique_ptr<data> setup_graph(const NNEDI3Weights &weights, const znedi3_filter_params &params, unsigned width, unsigned height, bool parity);
+public:
+	znedi3_filter2(const NNEDI3Weights &weights, const znedi3_filter_params &params, unsigned width, unsigned height);
+
+	~znedi3_filter2();
+
+	size_t get_tmp_size() const;
+
+	void process(const void *src, ptrdiff_t src_stride, void *dst, ptrdiff_t dst_stride, void *tmp, bool parity) const;
 };
 
 } // namespace znedi3
